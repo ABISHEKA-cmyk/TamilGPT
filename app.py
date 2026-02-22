dd18b8853b56273f537da9e2e18f05d794f72b6a
ABISHEKA-cmyk <abishekadavid2007@gmail.com>
2026-02-22 00:23:06 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="AnswerBot", page_icon="🤖", layout="wide")
 st.title("AnswerBot 🤖")
 st.markdown("Ungaloda smart Tamil AI assistant 🔥")
 user_input=user_input.lower()
-if user_input in ["hi","Hi","Hello","hello","hlo","Hey","Vanakkam"];
+if user_input in ["hi","Hi","Hello","hello","hlo","Hey","Vanakkam"]:
 response=["Hey vanakkam eppadi help pannanum?","Hi,solu ena pannanum?"]
 else:
 #normal LLM call
@@ -133,5 +133,6 @@ if col2.button("➤"):
 if st.button("Clear Chat 🗑️"):
     st.session_state.chat_history = []
     st.rerun()
+
 
 
