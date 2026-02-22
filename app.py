04498c8bc2568c7bc65140f0780bcb6c881a13d7
ABISHEKA-cmyk <abishekadavid2007@gmail.com>
2026-02-22 00:34:00 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ user_input=user_input.lower()
 if user_input in ["hi","Hi","Hello","hello","hlo","Hey","Vanakkam"]:
     response= "Hey vanakkam! eppadi help pannanum?"
 else:
-    #normal LLM call
+    response="na ungaluku help pana ready"
 
 # ---------------- BASIC KNOWLEDGE ---------------- #
 
@@ -133,6 +133,7 @@ if col2.button("➤"):
 if st.button("Clear Chat 🗑️"):
     st.session_state.chat_history = []
     st.rerun()
+
 
 
 
